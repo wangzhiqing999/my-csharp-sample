4b7841cca9d30af5e6f16cc081428f840fb392f5
wangzhiqing999 <wangzhiqing999@hotmail.com>
2016-12-14 10:03:30 +0800

    EF6_DbCommandInterceptor

--- A0622_EF_OneToMany/Sample/MyDbContext.cs
+++ A0622_EF_OneToMany/Sample/MyDbContext.cs
@@ -7,6 +7,13 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
 
+
+using System.Data.Entity.Infrastructure.Interception;
+using System.Diagnostics;
+
+
+
+
 using A0622_EF_OneToMany.Config;
 
 
@@ -20,6 +27,13 @@ namespace A0622_EF_OneToMany.Sample
         public MyDbContext()
             : base("name=MyDbContext")
         {
+
+
+            // 下面这句为 拦截的处理.
+#if DEBUG
+            DbInterception.Add(new EFIntercepterLogging());
+#endif
+
         }
 
 
@@ -70,5 +84,83 @@ namespace A0622_EF_OneToMany.Sample
         }
 
     }
+
+
+
+
+
+
+    /// <summary>
+    /// 这个类用于 EF6 中， 输出 EF 生成的 SQL 语句.
+    /// </summary>
+    class EFIntercepterLogging : DbCommandInterceptor
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+
+        public override void ScalarExecuting(System.Data.Common.DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            base.ScalarExecuting(command, interceptionContext);
+            _stopwatch.Restart();
+        }
+        public override void ScalarExecuted(System.Data.Common.DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            _stopwatch.Stop();
+            if (interceptionContext.Exception != null)
+            {
+                Trace.TraceError("Exception:{1} \r\n --> Error executing command: {0}", command.CommandText, interceptionContext.Exception.ToString());
+            }
+            else
+            {
+                Trace.TraceInformation("\r\n执行时间:{0} 毫秒\r\n-->ScalarExecuted.Command:{1}\r\n", _stopwatch.ElapsedMilliseconds, command.CommandText);
+            }
+            base.ScalarExecuted(command, interceptionContext);
+        }
+
+
+
+
+        public override void NonQueryExecuting(System.Data.Common.DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            base.NonQueryExecuting(command, interceptionContext);
+            _stopwatch.Restart();
+        }
+        public override void NonQueryExecuted(System.Data.Common.DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            _stopwatch.Stop();
+            if (interceptionContext.Exception != null)
+            {
+                Trace.TraceError("Exception:{1} \r\n --> Error executing command:\r\n {0}", command.CommandText, interceptionContext.Exception.ToString());
+            }
+            else
+            {
+                Trace.TraceInformation("\r\n执行时间:{0} 毫秒\r\n-->NonQueryExecuted.Command:\r\n{1}", _stopwatch.ElapsedMilliseconds, command.CommandText);
+            }
+            base.NonQueryExecuted(command, interceptionContext);
+        }
+
+
+
+
+        public override void ReaderExecuting(System.Data.Common.DbCommand command, DbCommandInterceptionContext<System.Data.Common.DbDataReader> interceptionContext)
+        {
+            base.ReaderExecuting(command, interceptionContext);
+            _stopwatch.Restart();
+        }
+        public override void ReaderExecuted(System.Data.Common.DbCommand command, DbCommandInterceptionContext<System.Data.Common.DbDataReader> interceptionContext)
+        {
+            _stopwatch.Stop();
+            if (interceptionContext.Exception != null)
+            {
+                Trace.TraceError("Exception:{1} \r\n --> Error executing command:\r\n {0}", command.CommandText, interceptionContext.Exception.ToString());
+            }
+            else
+            {
+                Trace.TraceInformation("\r\n执行时间:{0} 毫秒 \r\n -->ReaderExecuted.Command:\r\n{1}", _stopwatch.ElapsedMilliseconds, command.CommandText);
+            }
+            base.ReaderExecuted(command, interceptionContext);
+        }
+    }
+
 
 }
