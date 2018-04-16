22e8baf45959a44149e3cc7e1ecf7fd6b39fb9b6
wangzhiqing999 <wangzhiqing999@hotmail.com>
2018-04-16 17:39:06 +0800

    W1010_Mvc_Session Update

---
+++ W1010_Mvc_Session/Controllers/TestSessionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace W1010_Mvc_Session.Controllers
+{
+    public class TestSessionController : Controller
+    {
+        //
+        // GET: /TestSession/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string sessionValue) {
+
+            Session["TEST"] = sessionValue;
+
+            return View();
+        }
+
+    }
+}
