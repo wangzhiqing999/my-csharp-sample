142194774a73b55df597cf6802b3cf2a02b64fab
wangzhiqing999 <wangzhiqing999@hotmail.com>
2018-04-18 19:11:17 +0800

    JsonSerializerSettings Test Code

--- A0101_NewtonsoftJson/Sample/JsonConvertTest.cs
+++ A0101_NewtonsoftJson/Sample/JsonConvertTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 
 using A0101_NewtonsoftJson.Model;
@@ -31,14 +32,31 @@ namespace A0101_NewtonsoftJson.Sample
 
 
             string jsonString = JsonConvert.SerializeObject(testData);
-
             Console.WriteLine("对象转换为 Json 字符串：\n{0}", jsonString);
-
-
-
 
             DataObject testData2 = JsonConvert.DeserializeObject<DataObject>(jsonString);
             Console.WriteLine("Json 字符串 转换为 对象：\n{0}", testData2);
+
+
+
+            Console.WriteLine("------ 定义额外的设置 ------");
+
+            JsonSerializerSettings setting = new JsonSerializerSettings();
+            
+            // 使得序列化使用驼峰式大小写风格序列化属性
+            setting.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            // 将枚举类型在序列化时序列化字符串
+            setting.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            // 将日期类型，生成为  JavaScript 的日期.
+            setting.Converters.Add(new Newtonsoft.Json.Converters.JavaScriptDateTimeConverter());
+
+
+            jsonString = JsonConvert.SerializeObject(testData, setting);
+            Console.WriteLine("对象转换为 Json 字符串：\n{0}", jsonString);
+
+            testData2 = JsonConvert.DeserializeObject<DataObject>(jsonString, setting);
+            Console.WriteLine("Json 字符串 转换为 对象：\n{0}", testData2);
+
         }
 
 
