fd6fc9169560e45a185230542df457f84a1e4a97
wangzhiqing999 <wangzhiqing999@hotmail.com>
2019-10-25 21:13:31 +0800

    MVC5 404 Page Config

--- W1050_Mvc5/Controllers/TestErrorController.cs
+++ W1050_Mvc5/Controllers/TestErrorController.cs
@@ -16,6 +16,20 @@ namespace W1050_Mvc5.Controllers
 
 
 
+        /// <summary>
+        /// 测试页面返回的就是 HttpNotFound
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ResultHttpNotFound()
+        {
+            // 注意：
+            // 普通的 404 ， 通过 Global.asax.cs 中的 Application_Error 进行设置.
+            // HttpNotFound() 的 404，通过 Web.config 的 <system.webServer> 下面的 <httpErrors errorMode="Custom"> 进行设置.
+            return HttpNotFound();
+        }
+
+
+
 
         #region 404页面.
 
