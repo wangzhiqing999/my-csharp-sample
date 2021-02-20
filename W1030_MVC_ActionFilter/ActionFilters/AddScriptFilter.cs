8e2fcb15b9894c600df3be2ca91f1d4b8e689eb6
wangzhiqing999 <wangzhiqing999@hotmail.com>
2021-02-20 16:13:58 +0800

    修改 Filter, 将 脚本位置，由底部， 变更到 </head> 前

--- W1030_MVC_ActionFilter/ActionFilters/AddScriptFilter.cs
+++ W1030_MVC_ActionFilter/ActionFilters/AddScriptFilter.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.IO;
+using System.Text;
 
 namespace W1030_MVC_ActionFilter.ActionFilters
 {
@@ -14,44 +15,112 @@ namespace W1030_MVC_ActionFilter.ActionFilters
 
         private const string AddScript = @"
 <script>
-    console.log(' Test Script!');
+    console.log('我是一段 js 脚本，我将被安置在 html 的 </head> 的上面！');
 </script>
 ";
 
 
         void IResultFilter.OnResultExecuted(ResultExecutedContext filterContext)
         {
-            if (filterContext.Result.GetType().ToString() == "System.Web.Mvc.PartialViewResult")
+            if (filterContext.Result is ViewResult)
             {
-                // 忽略分布视图.
-                return;
+                // 仅仅处理视图的结果.
+
+                var response = filterContext.HttpContext.Response;
+
+
+                if (response.ContentType != "text/html" || response.Filter == null) return;
+
+
+                response.Filter = new AddScriptHelper(response.Filter);
             }
 
-            if (filterContext.Result.GetType().ToString() == "System.Web.Mvc.JsonResult")
-            {
-                // 忽略 Json .
-                return;
-            }
 
-            if (filterContext.Result.GetType().ToString() == "System.Web.Mvc.ContentResult")
-            {
-                // 忽略 Content .
-                return;
-            }
-
-            filterContext.RequestContext.HttpContext.Response.Write(AddScript);
+            
+            // 下面这种写法，是简单粗暴的，把 <script> 追加在 </html> 的后面.
+            // 不推荐使用.
+            // filterContext.RequestContext.HttpContext.Response.Write(AddScript);
             
         }
-
-
-
-
 
 
         void IResultFilter.OnResultExecuting(ResultExecutingContext filterContext)
         {
             // Do Nothing.
         }
+
+
+
+        private class AddScriptHelper : Stream
+        {
+            private readonly Stream _base;
+            StringBuilder _s = new StringBuilder();
+
+            public AddScriptHelper(Stream responseStream)
+            {
+                if (responseStream == null)
+                    throw new ArgumentNullException("responseStream");
+                _base = responseStream;
+            }
+
+            public override bool CanRead => _base.CanRead;
+
+            public override bool CanSeek => _base.CanSeek;
+
+            public override bool CanWrite => _base.CanWrite;
+
+            public override long Length => _base.Length;
+
+            public override long Position { get => _base.Position; set => _base.Position = value; }
+
+            public override void Flush()
+            {
+                _base.Flush();
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                return _base.Read(buffer, offset, count);
+            }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                return _base.Seek(offset, origin);
+            }
+
+            public override void SetLength(long value)
+            {
+                _base.SetLength(value);
+            }
+
+
+
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                var html = Encoding.UTF8.GetString(buffer, offset, count);
+
+                int headIndex = html.IndexOf("</head>");
+
+
+                if(headIndex > 0)
+                {
+                    string step0 = html.Substring(0, headIndex);
+                    string step2 = html.Substring(headIndex);
+
+                    html = string.Format(@"{0}
+{1}
+{2}", step0, AddScript, step2);
+                }
+
+
+
+                buffer = Encoding.UTF8.GetBytes(html);
+                _base.Write(buffer, 0, buffer.Length);
+            }
+        }
+
+
 
     }
 
