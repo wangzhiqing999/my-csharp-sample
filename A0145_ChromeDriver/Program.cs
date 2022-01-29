4a947da4299ca30d9e04f780f78ef04d1e544fb3
wangzhiqing999 <wangzhiqing999@hotmail.com>
2022-01-29 13:59:34 +0800

    Update A0145_ChromeDriver

--- A0145_ChromeDriver/Program.cs
+++ A0145_ChromeDriver/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 
 using OpenQA.Selenium.Chrome;
@@ -17,7 +18,9 @@ namespace A0145_ChromeDriver
         {
             // Test1();
 
-            Test2();
+            // Test2();
+
+            Test3();
 
             Console.WriteLine("Finish!");
             Console.ReadLine();
@@ -35,6 +38,8 @@ namespace A0145_ChromeDriver
 
 
 
+
+        
         static void Test2()
         {
             Console.WriteLine("=== Test2 ===");
@@ -52,6 +57,41 @@ namespace A0145_ChromeDriver
 
           
         }
+
+
+
+
+
+        /// <summary>
+        /// 运行 Test2， 可能会弹出， 要你转圈， 把一个图片 摆正的 验证操作.
+        /// 
+        /// 使用 chrome远程调试 模式.
+        /// </summary>
+        static void Test3()
+        {
+
+            // 先命令行运行
+            // chrome.exe --remote-debugging-port=9222
+
+
+            // 注：初步测试时，好像只能 处理本机的， 不能处理其它计算机的。
+            ChromeOptions opt = new ChromeOptions();
+            opt.DebuggerAddress = ConfigurationManager.AppSettings["ChromeDriver:DebuggerAddress"];
+            ChromeDriver driver = new ChromeDriver(ConfigurationManager.AppSettings["ChromeDriver:Path"], opt);
+
+
+            driver.Navigate().GoToUrl("https://www.baidu.com/");
+
+
+
+            var search = driver.FindElementById("kw");
+            search.Click();
+            search.SendKeys("browser");
+
+            var btn = driver.FindElementById("su");
+            btn.Click();
+        }
+
 
     }
 }
