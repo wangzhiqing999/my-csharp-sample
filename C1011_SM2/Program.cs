c8c4efe36801bf424dc965d2b783c7a741e906be
wangzhiqing999 <wangzhiqing999@hotmail.com>
2023-11-07 16:14:20 +0800

    Add C1011_SM2
    调用 SM2 加密解密的例子代码

---
+++ C1011_SM2/Program.cs
@@ -0,0 +1,165 @@
+﻿using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Utilities.Encoders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1011_SM2
+{
+    internal class Program
+    {
+
+
+        /// <summary>
+        /// 公钥.
+        /// <br/>
+        /// 可在网站生成一对公钥私钥.
+        /// 
+        /// https://lzltool.cn/SM2
+        /// </summary>
+        private const string _PublicKey = "04d300f5cebddb81eec359a80c810fea09158883e7724ea59b2187f6770f0fdae0fa5b2c5927b9545fecbc5d46fd5681dfdb511c2886dfc0c81c9513fce22ff6a8";
+
+
+        /// <summary>
+        /// 私钥.
+        /// </summary>
+        private const string _PrivateKey = "20da010ed7ee3308bb46d7d94d2c3568ec34109714e63679cd509beeef05517f";
+
+
+
+        /// <summary>
+        /// 老标准 C1C2C3 的密文数据顺序.
+        /// </summary>
+        private static SM2CryptoUtil sm2_c1c2c3 = new SM2CryptoUtil(Hex.Decode(_PublicKey), Hex.Decode(_PrivateKey), SM2CryptoUtil.Mode.C1C2C3);
+
+        /// <summary>
+        /// 新标准 C1C3C2 的密文数据顺序.
+        /// </summary>
+        private static SM2CryptoUtil sm2_c1c3c2 = new SM2CryptoUtil(Hex.Decode(_PublicKey), Hex.Decode(_PrivateKey), SM2CryptoUtil.Mode.C1C3C2);
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            // 测试 旧标准的【C1C2C3】， 自己加密，自己解密。
+            string test1 = Test_01("1234567890");
+            Test_02(test1);
+
+
+            // 测试新标准的【C1C3C2】， 自己加密，自己解密。
+            string test5 = Test_51("1234567890");
+            Test_52(test5);
+
+
+
+
+
+            // 测试 https://lzltool.cn/SM2 的旧标准的【C1C2C3】加密结果，本地能否完成解密.
+            // Test_02("043dc973d15d91db43da4701ea155ba01818e9c1e4f1e780b7b07fb3107f7525943e912515cf3db068813d99f4ed8d319afa1ec1e3cb28569b17b7a4ee372633d82962019d24fb802d440cf3bcba5088f0243185aafa0153381bd289c71909705aa33b9ac1cc29944f5938");
+            // 结果是解密过程发生异常.
+
+
+            // 测试 https://lzltool.cn/SM2 的新标准的【C1C3C2】加密结果，本地能否完成解密.
+            Test_52("04e844e45a82bbf5329815999723a39e4d0354f2092372974b8b7ad111b336245c5eb41e970e9d8af87c8aab56789555913651dec37d1fa3f23065fcf67d31ac74f4a08be41df87fe6faf8e67aa3938c6d3dc0c9ef7b912dee3b67c61bbf2121495680228b53d311dbbd79");
+            // 结果是解密成功.
+
+
+
+            Console.WriteLine("--- Finish ---");
+            Console.ReadLine();
+        }
+
+
+
+
+        /// <summary>
+        /// 老标准 C1C2C3 的 加密测试. 
+        /// 
+        /// 测试代码的加密结果， 在  https://lzltool.cn/SM2  网站上， 能够正确的完成解密。
+        /// </summary>
+        /// <param name="sourceText"></param>
+        /// <returns></returns>
+        static string Test_01(string sourceText)
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"加密：{sourceText}");
+
+            byte[] sourceData = Encoding.UTF8.GetBytes(sourceText);
+            byte[] cipherData = sm2_c1c2c3.Encrypt(sourceData);
+            string cipherText = Hex.ToHexString(cipherData);
+            
+            Console.WriteLine($"密文：{cipherText}");
+            Console.WriteLine();
+            return cipherText;
+        }
+
+
+        /// <summary>
+        /// 老标准 C1C2C3 的 解密测试.
+        /// </summary>
+        /// <param name="cipherText"></param>
+        /// <returns></returns>
+        static string Test_02(string cipherText)
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"解密：{cipherText}");
+
+            byte[] cipherData = Hex.Decode(cipherText);
+            byte[] plainData = sm2_c1c2c3.Decrypt(cipherData);
+            string plainText = Encoding.UTF8.GetString(plainData);
+
+            Console.WriteLine($"明文：{plainText}");
+            Console.WriteLine();
+            return plainText;
+        }
+
+
+
+
+        /// <summary>
+        /// 新标准 C1C3C2 的 加密测试. 
+        /// 
+        /// 测试代码的加密结果， 在  https://lzltool.cn/SM2  网站上， 能够正确的完成解密。
+        /// </summary>
+        /// <param name="sourceText"></param>
+        /// <returns></returns>
+        static string Test_51(string sourceText)
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"加密：{sourceText}");
+
+            byte[] sourceData = Encoding.UTF8.GetBytes(sourceText);
+            byte[] cipherData = sm2_c1c3c2.Encrypt(sourceData);
+            string cipherText = Hex.ToHexString(cipherData);
+
+            Console.WriteLine($"密文：{cipherText}");
+            Console.WriteLine();
+            return cipherText;
+        }
+
+
+        /// <summary>
+        /// 新标准 C1C3C2 的 解密测试. 
+        /// </summary>
+        /// <param name="cipherText"></param>
+        /// <returns></returns>
+        static string Test_52(string cipherText)
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"解密：{cipherText}");
+
+            byte[] cipherData = Hex.Decode(cipherText);
+            byte[] plainData = sm2_c1c3c2.Decrypt(cipherData);
+            string plainText = Encoding.UTF8.GetString(plainData);
+
+            Console.WriteLine($"明文：{plainText}");
+            Console.WriteLine();
+            return plainText;
+        }
+
+    }
+}
---
+++ C1011_SM2/SM2CryptoUtil.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+using Org.BouncyCastle.Asn1.GM;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities.Encoders;
+
+
+
+namespace C1011_SM2
+{
+
+    /// <summary>
+    /// Copy From
+    /// https://gitee.com/Karl_Albright/CryptoHelper/blob/master/src/CryptoHelper/SM2CryptoUtil.cs
+    /// </summary>
+    public class SM2CryptoUtil
+    {
+        public SM2CryptoUtil(byte[] pubkey, byte[] privkey, Mode mode)
+        {
+            this.pubkey = pubkey;
+            this.privkey = privkey;
+            this.mode = mode;
+        }
+        public SM2CryptoUtil(string pubkey, string privkey, Mode mode = Mode.C1C2C3, bool isPkcs8 = false)
+        {
+            if (!isPkcs8)
+            {
+                if (pubkey != null) this.pubkey = Decode(pubkey);
+                if (privkey != null) this.privkey = Decode(privkey);
+            }
+            else
+            {
+                if (pubkey != null) this.pubkey = ((ECPublicKeyParameters)PublicKeyFactory.CreateKey(Convert.FromBase64String(pubkey))).Q.GetEncoded();
+                if (privkey != null) this.privkey = ((ECPrivateKeyParameters)PrivateKeyFactory.CreateKey(Convert.FromBase64String(privkey))).D.ToByteArray();
+            }
+            this.mode = mode;
+        }
+        byte[] pubkey;
+        byte[] privkey;
+        Mode mode;
+        ICipherParameters _privateKeyParameters;
+        ICipherParameters PrivateKeyParameters
+        {
+            get
+            {
+                var r = _privateKeyParameters;
+                if (r == null) r = _privateKeyParameters = new ECPrivateKeyParameters(new BigInteger(1, privkey), new ECDomainParameters(GMNamedCurves.GetByName("SM2P256V1")));
+                return r;
+            }
+        }
+        ICipherParameters _publicKeyParameters;
+        ICipherParameters PublicKeyParameters
+        {
+            get
+            {
+                var r = _publicKeyParameters;
+                if (r == null)
+                {
+                    var x9ec = GMNamedCurves.GetByName("SM2P256V1");
+                    r = _publicKeyParameters = new ECPublicKeyParameters(x9ec.Curve.DecodePoint(pubkey), new ECDomainParameters(x9ec));
+                }
+                return r;
+            }
+        }
+
+        public static void GenerateKeyHex(out string pubkey, out string privkey)
+        {
+            GenerateKey(out var a, out var b);
+            pubkey = Hex.ToHexString(a);
+            privkey = Hex.ToHexString(b);
+        }
+        public static void GenerateKey(out byte[] pubkey, out byte[] privkey)
+        {
+            var g = new ECKeyPairGenerator();
+            g.Init(new ECKeyGenerationParameters(new ECDomainParameters(GMNamedCurves.GetByName("SM2P256V1")), new SecureRandom()));
+            var k = g.GenerateKeyPair();
+            pubkey = ((ECPublicKeyParameters)k.Public).Q.GetEncoded(false);
+            privkey = ((ECPrivateKeyParameters)k.Private).D.ToByteArray();
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            if (mode == Mode.C1C3C2) data = C132ToC123(data);
+            var sm2 = new SM2Engine(new SM3Digest());
+            sm2.Init(false, this.PrivateKeyParameters);
+            return sm2.ProcessBlock(data, 0, data.Length);
+        }
+        public byte[] Encrypt(byte[] data)
+        {
+            var sm2 = new SM2Engine(new SM3Digest());
+            sm2.Init(true, new ParametersWithRandom(PublicKeyParameters));
+            data = sm2.ProcessBlock(data, 0, data.Length);
+            if (mode == Mode.C1C3C2) data = C123ToC132(data);
+            return data;
+        }
+        public byte[] Sign(byte[] msg, byte[] id = null)
+        {
+            var sm2 = new SM2Signer(new SM3Digest());
+            ICipherParameters cp;
+            if (id != null) cp = new ParametersWithID(new ParametersWithRandom(PrivateKeyParameters), id);
+            else cp = new ParametersWithRandom(PrivateKeyParameters);
+            sm2.Init(true, cp);
+            sm2.BlockUpdate(msg, 0, msg.Length);
+            return sm2.GenerateSignature();
+        }
+        public bool VerifySign(byte[] msg, byte[] signature, byte[] id = null)
+        {
+            var sm2 = new SM2Signer(new SM3Digest());
+            ICipherParameters cp;
+            if (id != null) cp = new ParametersWithID(PublicKeyParameters, id);
+            else cp = PublicKeyParameters;
+            sm2.Init(false, cp);
+            sm2.BlockUpdate(msg, 0, msg.Length);
+            return sm2.VerifySignature(signature);
+        }
+        static byte[] C123ToC132(byte[] c1c2c3)
+        {
+            var gn = GMNamedCurves.GetByName("SM2P256V1");
+            int c1Len = (gn.Curve.FieldSize + 7) / 8 * 2 + 1; //sm2p256v1的这个固定65。可看GMNamedCurves、ECCurve代码。
+            int c3Len = 32; //new SM3Digest().getDigestSize();
+            byte[] result = new byte[c1c2c3.Length];
+            Array.Copy(c1c2c3, 0, result, 0, c1Len); //c1
+            Array.Copy(c1c2c3, c1c2c3.Length - c3Len, result, c1Len, c3Len); //c3
+            Array.Copy(c1c2c3, c1Len, result, c1Len + c3Len, c1c2c3.Length - c1Len - c3Len); //c2
+            return result;
+        }
+        static byte[] C132ToC123(byte[] c1c3c2)
+        {
+            var gn = GMNamedCurves.GetByName("SM2P256V1");
+            int c1Len = (gn.Curve.FieldSize + 7) / 8 * 2 + 1;
+            int c3Len = 32; //new SM3Digest().getDigestSize();
+            byte[] result = new byte[c1c3c2.Length];
+            Array.Copy(c1c3c2, 0, result, 0, c1Len); //c1: 0->65
+            Array.Copy(c1c3c2, c1Len + c3Len, result, c1Len, c1c3c2.Length - c1Len - c3Len); //c2
+            Array.Copy(c1c3c2, c1Len, result, c1c3c2.Length - c3Len, c3Len); //c3
+            return result;
+        }
+        static byte[] Decode(string key)
+        {
+            return Regex.IsMatch(key, "^[0-9a-f]+$", RegexOptions.IgnoreCase) ? Hex.Decode(key) : Convert.FromBase64String(key);
+        }
+        public enum Mode
+        {
+            C1C2C3, C1C3C2
+        }
+    }
+}
